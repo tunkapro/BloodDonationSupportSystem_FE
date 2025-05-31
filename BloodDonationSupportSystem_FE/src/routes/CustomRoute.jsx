f6c03dfa27ffa73aadac8ab1d5257425b74e30cc
Anh Tuan <truongtuan20042004@gmail.com>
2025-06-01 00:29:22 +0700

    fix folder

--- BloodDonationSupportSystem_FE/src/routes/CustomRoute.jsx
+++ BloodDonationSupportSystem_FE/src/routes/CustomRoute.jsx
@@ -1,37 +1,37 @@
 import { Route, Routes } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
 import Home from "../pages/DefautPage/HomePage/Home.component";
+import Contact from "../pages/DefautPage/ContactPage/Contact";
 import News from "../pages/DefautPage/NewsPage/News";
 import QuestionAndAnswer from "../pages/DefautPage/Q&APage/Q&APage";
-import Contact from "../pages/DefautPage/ContactPage/Contact";
 import LoginPage from "../pages/DefautPage/LoginPage/LoginPage";
-import RegisterPage from "../pages/DefautPage/RegisterPage/RegisterPage";
 import ForgotPasswordPage from "../pages/DefautPage/ForgotPasswordPage/ForgotPassword";
+import RegisterPage from "../pages/DefautPage/RegisterPage/RegisterPage";
 import MemberLayout from "../layouts/MemberLayout/MemberLayout";
-import StaffLayout from "../layouts/StaffLayout/StaffLayout";
+import BloodDonateHistory from "../pages/MemberPage/BloodDonateHistoryPage/BloodDonateHistory";
 const CustomRoute = () => {
     return (
         <Routes>
-            <Route path="/" element={<DefaultLayout/>}>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/news" element={<News/>}/>
-                <Route path="/q-a" element={<QuestionAndAnswer/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/reset-password" element={<ForgotPasswordPage/>}/>
-                
-                <Route path="/user" element={<MemberLayout />}>
-                    <Route path=""></Route>
+            <Route path="/" element={<DefaultLayout />}>
+                <Route index element={<Home />} />
+                <Route path="Home" element={<Home />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="news" element={<News />} />
+                <Route path="q-a" element={<QuestionAndAnswer/>} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="reset-password" element={<ForgotPasswordPage />} />
+                <Route path="signup" element={<RegisterPage />} />
+                <Route path="/user" element={<MemberLayout/>}>
+                    <Route path="histories" element={<BloodDonateHistory/>}/>
                 </Route>
             </Route>
-            <Route path="/staff/*" element={<StaffLayout />}>
-                {/* Các route staff khác */}
-                {/* <Route index element={<Overview />} />
+           
+            {/* <Route path="/staff/*" element={<StaffLayout />}>
+               
+                <Route index element={<Overview />} />
                 <Route path="overview" element={<Overview />} />
-                <Route path="storage" element={<BloodStorageTable />} /> */}
-            </Route>
+                <Route path="storage" element={<BloodStorageTable />} />
+            </Route> */}
         </Routes>
     );
 };
--- src/api/ManagementAPI.jsx
+++
@@ -1,5 +0,0 @@
-
-
-export const ManagementAPI = {
-    // chứa tất cả API ở đây để sử dụng. Đặt tạo class đặt static function cũng đc.
-}
