f5db14dc12a360ba13fc4dc8ed5120e52dd94058
Quang-keri <phamquang040102@gmail.com>
2025-06-09 14:07:42 +0700

    create paging for inventory and search by blood type

--- BloodDonationSupportSystem_FE/src/components/staff/BloodStorageTable.jsx
+++ BloodDonationSupportSystem_FE/src/components/staff/BloodStorageTable.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
-  AppBar, Toolbar, Typography, Box, Button,
+  AppBar, Toolbar, Typography, Box, Button,TextField,
   Container, Paper, TableContainer, Table,
-  TableHead, TableRow, TableCell, TableBody
+  TableHead, TableRow, TableCell, TableBody,TablePagination
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
 function BloodStorageTable({ data,onViewDashBoard,onCreateBloodBag}) {
  const navigate = useNavigate();
+ const [page, setPage] = useState(0);
+ const [rowsPerPage, setRowsPerPage] = useState(8);
+ const handleChangePage = (event, newPage) => {
+  setPage(newPage);
+};
+const handleChangeRowsPerPage = (event) => {
+  setRowsPerPage(+event.target.value);
+  setPage(0);
+};
+
+const [searchText, setSearchText] = useState('');
+// data dồn vào filter này lọc ra tui máu theo nhóm data,mặc đinh là full nếu ko search
+const filteredRows = data.filter((row) =>
+  row.nhom.toLowerCase().includes(searchText.toLowerCase())
+);
+
   return (
     <Box>
       <AppBar position="static">
@@ -21,9 +36,12 @@ function BloodStorageTable({ data,onViewDashBoard,onCreateBloodBag}) {
           <Button color="inherit">Xuất bịch máu</Button>
         </Toolbar>
       </AppBar>
-
-      <Container sx={{ mt: 2 }}>
-        <TableContainer component={Paper}>
+      <Box sx={{textAlign:'center'}}>
+        <TextField sx={{mt:'5px',width:'500px'}} variant="outlined" size="small" onChange={(e) => setSearchText(e.target.value)}  placeholder='Tìm kiếm nhóm máu..'/>
+      </Box>
+     
+      <Container sx={{ mt: 1 }}>
+        <TableContainer component={Paper} >
           <Table>
             <TableHead>
               <TableRow>
@@ -39,9 +57,11 @@ function BloodStorageTable({ data,onViewDashBoard,onCreateBloodBag}) {
               </TableRow>
             </TableHead>
             <TableBody>
-              {data.map((row,index) => (
+              {filteredRows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row,index) => (
                 <TableRow key={row.tui}>
-                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{page * rowsPerPage + index + 1}</TableCell>
                   <TableCell>{row.tui}</TableCell>
                   <TableCell>{row.nhom}</TableCell>
                   <TableCell>{row.dungTich}</TableCell>
@@ -55,6 +75,19 @@ function BloodStorageTable({ data,onViewDashBoard,onCreateBloodBag}) {
             </TableBody>
           </Table>
         </TableContainer>
+        <TablePagination
+         rowsPerPageOptions={[]}
+        component="div"
+        count={filteredRows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        labelDisplayedRows={({ from, to, count, page }) =>
+          `Trang ${page + 1} trên ${Math.ceil(count / rowsPerPage)}`
+        }
+       
+      />
       </Container>
     </Box>
   );
--- BloodDonationSupportSystem_FE/src/pages/StaffPage/BloodStoragePage.jsx
+++ BloodDonationSupportSystem_FE/src/pages/StaffPage/BloodStoragePage.jsx
@@ -8,7 +8,19 @@ function BloodStoragePage() {
       { tui: 'b01',nhom: 'A+',dungTich: '450',soluong: '1', nhap: '10/5/2025', hetHan: '10/6/2025', trangThai: 'Đã nhận',maHienMau:'abc123456'},
       { tui: 'b02',nhom: 'A+',dungTich: '400',soluong: '1', nhap: '10/5/2025', hetHan: '10/6/2025', trangThai: 'Đã kiểm tra',maHienMau:'abc123457'},
       { tui: 'b03',nhom: 'B+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/5/2025', trangThai: 'Hết hạn',maHienMau:'abc123458'},
-      { tui: 'b04',nhom: 'O+',dungTich: '450',soluong: '1', nhap: '12/5/2025', hetHan: '12/6/2025', trangThai: 'Không đạt',maHienMau:'abc123459'}
+      { tui: 'b04',nhom: 'O+',dungTich: '450',soluong: '1', nhap: '12/5/2025', hetHan: '12/6/2025', trangThai: 'Không đạt',maHienMau:'abc123459'},
+      { tui: 'b05',nhom: 'B+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/5/2025', trangThai: 'Hết hạn',maHienMau:'abc223458'},
+      { tui: 'b06',nhom: 'O+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Đã kiểm tra',maHienMau:'abc323458'},
+      { tui: 'b07',nhom: 'B+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/5/2025', trangThai: 'Hết hạn',maHienMau:'abc423458'},
+      { tui: 'b08',nhom: 'AB-+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/5/2025', trangThai: 'Hết hạn',maHienMau:'abc523458'},
+      { tui: 'b09',nhom: 'B+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Hết hạn',maHienMau:'abc623458'},
+      { tui: 'b10',nhom: 'B+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Hết hạn',maHienMau:'abc723458'},
+      { tui: 'b11',nhom: 'AB+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Hết hạn',maHienMau:'abc823458'},
+      { tui: 'b12',nhom: 'AB+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Đã nhận',maHienMau:'abc923458'},
+      { tui: 'b13',nhom: 'AB-',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/6/2025', trangThai: 'Đã nhận',maHienMau:'abc1023458'},
+      { tui: 'b14',nhom: 'O+',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/5/2025', trangThai: 'Đã kiểm tra',maHienMau:'abc113458'},
+      { tui: 'b15',nhom: 'O-',dungTich: '450',soluong: '1', nhap: '11/4/2025', hetHan: '11/7/2025', trangThai: 'Không đạt',maHienMau:'abc133458'}
+
       
     ];
     const navigate = useNavigate();
