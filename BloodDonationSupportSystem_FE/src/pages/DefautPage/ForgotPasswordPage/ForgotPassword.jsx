2d280ffb935b2f397845e06783204de4b80cfa7b
OverCode <saoran123456@gmail.com>
2025-07-29 01:20:43 +0700

    Update ForgotPassword.jsx

--- BloodDonationSupportSystem_FE/src/pages/DefautPage/ForgotPasswordPage/ForgotPassword.jsx
+++ BloodDonationSupportSystem_FE/src/pages/DefautPage/ForgotPasswordPage/ForgotPassword.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { ArrowRight, ArrowLeft, CheckCircle } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import OtpInput from '../../../components/OTPInput';
 
 
 export default function ForgotPasswordPage() {
